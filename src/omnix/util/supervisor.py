223fe18d873f7250b6daf34b2a97c2a72d6c1b00
João Magalhães <joamag@gmail.com>
2024-04-05 11:31:32 +0100

    fix: code syntax issue

--- src/omnix/util/supervisor.py
+++ src/omnix/util/supervisor.py
@@ -60,6 +60,7 @@ NUMBER_RECORDS = config.RECORD_CHUNK
 retrieved and set for submission, this value influences
 the performance, mostly under heavy load situations """
 
+
 class Supervisor(threading.Thread):
 
     session_id = None
@@ -75,75 +76,84 @@ class Supervisor(threading.Thread):
         pass
 
     def auth(self):
-        if not config.REMOTE: return
+        if not config.REMOTE:
+            return
 
         username = config.USERNAME
         password = config.PASSWORD
         if username == None or password == None:
             raise RuntimeError("Missing authentication information")
 
-        self.api = logic.get_api(mode = omni.API.DIRECT_MODE)
+        self.api = logic.get_api(mode=omni.API.DIRECT_MODE)
 
-    def connect(self, queue = "default", retry = True):
-        if not config.REMOTE: return
+    def connect(self, queue="default", retry=True):
+        if not config.REMOTE:
+            return
 
         quorum.info("Connecting to the AMQP system")
 
         while True:
             try:
-                self.connection = quorum.get_amqp(force = True)
+                self.connection = quorum.get_amqp(force=True)
                 self.channel = self.connection.channel()
-                self.channel.queue_declare(queue = queue, durable = True)
+                self.channel.queue_declare(queue=queue, durable=True)
                 self.queue = queue
                 break
             except Exception as exception:
-                if not retry: raise
+                if not retry:
+                    raise
                 quorum.error(
-                    "Exception while connecting - %s" % quorum.legacy.UNICODE(exception),
-                    log_trace = True
+                    "Exception while connecting - %s"
+                    % quorum.legacy.UNICODE(exception),
+                    log_trace=True,
                 )
                 quorum.info("Sleeping %d seconds before connect retry" % RETRY_TIMEOUT)
                 time.sleep(RETRY_TIMEOUT)
 
     def disconnect(self):
-        if not config.REMOTE: return
+        if not config.REMOTE:
+            return
 
         quorum.info("Disconnected from the AMQP system")
 
         self.connection.close()
 
-    def reconnect(self, retry = True):
-        if not config.REMOTE: return
-        if not self.connection.is_closed: return
+    def reconnect(self, retry=True):
+        if not config.REMOTE:
+            return
+        if not self.connection.is_closed:
+            return
 
         quorum.info("Re-connecting to the AMQP system")
 
-        self.connect(queue = self.queue, retry = retry)
+        self.connect(queue=self.queue, retry=retry)
 
     def execute(self):
         # in case the current instance is not configured according to
         # the remote rules the queuing operation is ignored, and so
         # the control flow returns immediately
-        if not config.REMOTE: return
+        if not config.REMOTE:
+            return
 
         # creates a values map structure to retrieve the complete
         # set of inbound documents that have not yet been submitted
         # to at for the flush operation
         kwargs = {
-            "session_id" : self.session_id,
-            "filter_string" : "",
-            "start_record" : 0,
-            "number_records" : NUMBER_RECORDS,
-            "sort" : "issue_date:ascending",
-            "filters[]" : [
+            "session_id": self.session_id,
+            "filter_string": "",
+            "start_record": 0,
+            "number_records": NUMBER_RECORDS,
+            "sort": "issue_date:ascending",
+            "filters[]": [
                 "issue_date:greater:1356998400",
                 "submitted_at:equals:2",
-                "document_type:in:1;3"
-            ]
+                "document_type:in:1;3",
+            ],
         }
         documents = self.api.list_signed_documents(**kwargs)
-        valid_documents = [value for value in documents\
-            if value["_class"] in config.AT_SUBMIT_TYPES]
+        valid_documents = [
+            value for value in documents if value["_class"] in config.AT_SUBMIT_TYPES
+        ]
 
         # starts the counter value to zero, so that we're able to count
         # the number of messages that have been successfully queued to
@@ -152,7 +162,10 @@ class Supervisor(threading.Thread):
 
         # prints a debug message about the number of valid documents that
         # have been found for submission to the queue
-        quorum.debug("Found %d valid documents for submission, from a total of %d documents" % (len(valid_documents), len(documents))
+        quorum.debug(
+            "Found %d valid documents for submission, from a total of %d documents"
+            % (len(valid_documents), len(documents))
+        )
 
         # iterates over all the valid documents that have been found
         # as not submitted and creates a task for their submission
@@ -164,23 +177,24 @@ class Supervisor(threading.Thread):
                 # underlying library so a reconnection is attempted in case
                 # there's an exception raised under this operation
                 self.channel.basic_publish(
-                    exchange = "",
-                    routing_key = config.QUEUE,
-                    body = json.dumps(document),
-                    properties = quorum.properties_amqp(
-                        delivery_mode = 2,
-                        priority = MESSAGE_RETRIES,
-                        expiration = str(MESSAGE_TIMEOUT * 1000),
-                        timestamp = time.time()
-                    )
+                    exchange="",
+                    routing_key=config.QUEUE,
+                    body=json.dumps(document),
+                    properties=quorum.properties_amqp(
+                        delivery_mode=2,
+                        priority=MESSAGE_RETRIES,
+                        expiration=str(MESSAGE_TIMEOUT * 1000),
+                        timestamp=time.time(),
+                    ),
                 )
                 count += 1
             except Exception as exception:
                 # prints a warning message about the exception that has just occurred
                 # so that it's possible to act on it
                 quorum.warning(
-                    "Exception in publish (will re-connect) - %s" % quorum.legacy.UNICODE(exception),
-                    log_trace = True
+                    "Exception in publish (will re-connect) - %s"
+                    % quorum.legacy.UNICODE(exception),
+                    log_trace=True,
                 )
 
                 # re-tries to connect with the AMQP channels using the currently
@@ -189,17 +203,21 @@ class Supervisor(threading.Thread):
 
         # prints an information message about the new documents that
         # have been queued for submission by the "slaves"
-        quorum.info("Queued %d (out of %d) documents for submission" % (count, len(valid_documents)))
+        quorum.info(
+            "Queued %d (out of %d) documents for submission"
+            % (count, len(valid_documents))
+        )
 
     def loop(self):
         while True:
-            try: self.execute()
+            try:
+                self.execute()
             except Exception as exception:
                 # prints an error message about the exception that has just occurred
                 # so that it's possible to act on it
                 quorum.error(
                     "Exception while executing - %s" % quorum.legacy.UNICODE(exception),
-                    log_trace = True
+                    log_trace=True,
                 )
 
                 # re-tries to connect with the AMQP channels using the currently
@@ -207,14 +225,16 @@ class Supervisor(threading.Thread):
                 self.reconnect()
 
             try:
-                if self.connection: self.connection.sleep(LOOP_TIMEOUT)
-                else: time.sleep(LOOP_TIMEOUT)
+                if self.connection:
+                    self.connection.sleep(LOOP_TIMEOUT)
+                else:
+                    time.sleep(LOOP_TIMEOUT)
             except Exception as exception:
                 # prints a critical message about the exception that has just occurred
                 # so that it's possible to act on it
                 quorum.critical(
                     "Exception while sleeping - %s" % quorum.legacy.UNICODE(exception),
-                    log_trace = True
+                    log_trace=True,
                 )
 
                 # re-tries to connect with the AMQP channels using the currently
@@ -223,11 +243,14 @@ class Supervisor(threading.Thread):
 
     def run(self):
         self.auth()
-        self.connect(queue = config.QUEUE)
-        try: self.loop()
-        finally: self.disconnect()
+        self.connect(queue=config.QUEUE)
+        try:
+            self.loop()
+        finally:
+            self.disconnect()
 
-def run(count = 1):
+
+def run(count=1):
     for _index in range(count):
         supervisor = Supervisor()
         supervisor.start()
